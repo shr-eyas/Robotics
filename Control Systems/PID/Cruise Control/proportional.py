b8e95f70abe790daa99b6e8dca290f6e4bec50e9
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-06-19 19:23:21 +0530

    Create proportional.py

---
+++ Control Systems/PID/Cruise Control/proportional.py
@@ -0,0 +1,29 @@
+import numpy as np
+import control
+import matplotlib.pyplot as plt
+
+m = 1000  # Mass
+b = 50    # Damping coefficient
+r = 10    # Reference input
+
+# Transfer function of the plant (cruise control system)
+
+s = control.TransferFunction.s
+P_cruise = 1 / (m * s + b)
+
+Kp = 100     # Proportional gain
+C = Kp
+
+# Closed-loop transfer function
+T = control.feedback(C * P_cruise, 1)
+
+t = np.arange(0, 20, 0.1)
+t, y = control.step_response(r * T, T=t)
+
+plt.plot(t, y)
+plt.axis([0, 20, 0, 10])
+plt.xlabel('Time')
+plt.ylabel('Output')
+plt.title('Step Response')
+plt.grid(True)
+plt.show()
