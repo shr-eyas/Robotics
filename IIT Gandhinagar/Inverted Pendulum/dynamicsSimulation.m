60aef1d263aeb579fd94975679c7d3bab2207306
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-07-26 00:56:10 +0530

    Update dynamicsSimulation.m

--- IIT Gandhinagar/Inverted Pendulum/dynamicsSimulation.m
+++ IIT Gandhinagar/Inverted Pendulum/dynamicsSimulation.m
@@ -1,9 +1,13 @@
+% Pendulum parameters
 m = 1;              % Mass = 1kg
 L = 1;              % Length = 1m
-t_span = [0, 10];   % Time (t=0 to t=10 seconds)
+
+% Time parameters
+run_time = 10;
+t_span = [0, run_time]; 
 
 % Initial state [position; angular velocity]
-initial_state = [0; 0]; 
+initial_state = [pi/2; 0]; 
 
 % Torque input
 tau = 0;  
@@ -11,6 +15,8 @@ tau = 0;
 % x is the state vector [position; angular velocity]
 [t, x] = ode45(@(t, x) pendulumDynamics(t, x, m, L, tau), t_span, initial_state);
 
+% Convert angles from radians to degrees and wrap between 0 and 360 degrees
+x(:, 1) = (x(:, 1));
 
 figure;
 subplot(2, 1, 1);
