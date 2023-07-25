a758ebada80dd1045fe4b9fe9172100ed2b9b0df
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-07-25 23:47:20 +0530

    Update pendulumDynamics.m

--- IIT Gandhinagar/Inverted Pendulum/pendulumDynamics.m
+++ IIT Gandhinagar/Inverted Pendulum/pendulumDynamics.m
@@ -1,7 +1,5 @@
-function dx = pendulum_dynamics(t, x, tau)
-    m = 1;
-    L = 0.5;
+function dx = pendulum_dynamics(~, x, m, L, tau)
     g = 9.81;
-
-    dx = [x(2); -(g/L)*x(1)] + [0; (1/m*l^2)*tau];
+    dx = [x(2); -(g/L)*sin(x(1))] + [0; (1/m*L^2)*tau];
 end
+
