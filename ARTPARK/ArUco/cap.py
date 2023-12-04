ca7174f5e1efcc2b6c6d8f180b3fbf6ff52ccde3
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-12-04 12:01:21 +0530

    Update and rename video.py to cap.py

---
+++ ARTPARK/ArUco/cap.py
@@ -0,0 +1,19 @@
+# code to capture either the image or the video
+import cv2
+import cv2.aruco as arcuo
+
+VideoCap = False
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    if VideoCap: 
+        _,img=cap.read()
+    else: 
+        img = cv2.imread('ARTPARK/1.png')
+        img = cv2.resize(img,(0,0),fx=0.5,fy=0.5)
+        
+    if cv2.waitKey(1)==113:
+        break   
+
+    cv2.imshow('img',img)
