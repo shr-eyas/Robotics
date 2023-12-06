6ab9c0627caf4092dc04ef1107bb67ca59041afe
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-12-06 10:11:07 +0530

    Delete ARTPARK/ArUco/check.py

--- ARTPARK/ArUco/check.py
+++
@@ -1,8 +0,0 @@
-# code to check if the path of the file is correct
-import cv2
-import sys
-
-img = cv2.imread('ARTPARK/1.png')
-if img is None:
-    print("Error: Unable to load image. Check the file path.")
-    sys.exit(1)  # or exit(1)
