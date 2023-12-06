e1a6574690cd4ba3538d624aad1a24934f926fef
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-12-06 10:10:23 +0530

    Create create.py

---
+++ ARTPARK/ArUco/create.py
@@ -0,0 +1,10 @@
+import cv2
+
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+marker_id = 1
+marker_size = 200
+border_bits = 1
+
+markerImage = cv2.aruco.drawMarker(dictionary, marker_id, marker_size, borderBits=border_bits)
+
+cv2.imwrite('marker2.png', markerImage)
